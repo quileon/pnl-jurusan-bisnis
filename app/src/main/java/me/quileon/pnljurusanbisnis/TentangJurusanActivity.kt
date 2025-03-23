09a75ed395ace0cc8f9898bec63d4ab781c6fda8
Quileon <121283381+quileon@users.noreply.github.com>
2025-03-23 23:17:07 +0700

    Fix force close on opening `TentangJurusanActivity.kt`

--- app/src/main/java/me/quileon/pnljurusanbisnis/TentangJurusanActivity.kt
+++ app/src/main/java/me/quileon/pnljurusanbisnis/TentangJurusanActivity.kt
@@ -12,11 +12,11 @@ class TentangJurusanActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tentang_jurusan)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tentang_jurusan_layout)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
 
         val backBtn = findViewById<ImageButton>(R.id.btn_back)
         backBtn.setOnClickListener {
